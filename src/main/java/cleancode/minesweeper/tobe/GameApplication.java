6a7b7546d7ea09bcd2b512623bb171f2af65e04b
geneyee <geneyeey@gmail.com>
2024-10-08 21:08:27 +0900

    Refactor: [section04] SOLID - DIP

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -4,15 +4,35 @@ import cleancode.minesweeper.tobe.gamelevel.Advanced;
 import cleancode.minesweeper.tobe.gamelevel.Beginner;
 import cleancode.minesweeper.tobe.gamelevel.GameLevel;
 import cleancode.minesweeper.tobe.gamelevel.Middle;
+import cleancode.minesweeper.tobe.io.ConsoleInputHandler;
+import cleancode.minesweeper.tobe.io.ConsoleOutputHandler;
+import cleancode.minesweeper.tobe.io.InputHandler;
+import cleancode.minesweeper.tobe.io.OutputHandler;
 
 public class GameApplication {
 
     public static void main(String[] args) {
-//        GameLevel gameLevel = new Beginner();
+        GameLevel gameLevel = new Beginner();
 //        GameLevel gameLevel = new Middle();
-        GameLevel gameLevel = new Advanced();
-        Minesweeper minesweeper = new Minesweeper(gameLevel);
+//        GameLevel gameLevel = new Advanced();
+        InputHandler inputHandler = new ConsoleInputHandler();
+        OutputHandler outputHandler = new ConsoleOutputHandler();
+
+        Minesweeper minesweeper = new Minesweeper(gameLevel, inputHandler, outputHandler);
         minesweeper.initialize();
         minesweeper.run();
     }
+
+    /** 주의
+     * DIP(Dependency Inversion Principle) : 의존성 역전
+     *
+     * DI(Dependency Injection) : 의존성 주입
+     * -> 필요한 의존성을 내가 직접 생성하는 것이 아니라 외부에서 주입
+     * -> a객체를 b객체에서 주입받아서 사용하고 싶다고 할 때, 직접 주입 받을 수 없음.
+     * -> 스프링에서는 이것을 spring context가 해준다.
+     *
+     * IoC(Inversion of Control) : 제어의 역전
+     * -> 프로그램의 흐름을 프레임워크가 제어하도록 하는 것.(생명주기 - 객체의 생성, 소멸)
+     * -> 순방향은 개발자가 제어하는 것
+    * */
 }
--- src/main/java/cleancode/minesweeper/tobe/Minesweeper.java
+++ src/main/java/cleancode/minesweeper/tobe/Minesweeper.java
@@ -5,17 +5,21 @@ import cleancode.minesweeper.tobe.game.GameRunnable;
 import cleancode.minesweeper.tobe.gamelevel.GameLevel;
 import cleancode.minesweeper.tobe.io.ConsoleInputHandler;
 import cleancode.minesweeper.tobe.io.ConsoleOutputHandler;
+import cleancode.minesweeper.tobe.io.InputHandler;
+import cleancode.minesweeper.tobe.io.OutputHandler;
 
 public class Minesweeper implements GameInitializable, GameRunnable {
 
     private final GameBoard gameBoard;
     private final BoardIndexConverter boardIndexConverter = new BoardIndexConverter();
-    private final ConsoleInputHandler consoleInputHandler = new ConsoleInputHandler();
-    private final ConsoleOutputHandler consoleOutputHandler = new ConsoleOutputHandler();
+    private final InputHandler inputHandler;
+    private final OutputHandler outputHandler;
     private int gameStatus = 0; // 0: 게임 중, 1: 승리, -1: 패배
 
-    public Minesweeper(GameLevel gameLevel) {
+    public Minesweeper(GameLevel gameLevel, InputHandler inputHandler, OutputHandler outputHandler) {
         gameBoard = new GameBoard(gameLevel);
+        this.inputHandler = inputHandler;
+        this.outputHandler = outputHandler;
     }
 
     @Override
@@ -25,37 +29,38 @@ public class Minesweeper implements GameInitializable, GameRunnable {
 
     @Override
     public void run() {
-        consoleOutputHandler.showGameStartComments();
+        outputHandler.showGameStartComments();
 
         while (true) {
             try {
-                consoleOutputHandler.showBoard(gameBoard);
+                outputHandler.showBoard(gameBoard);
 
                 if (doesUserWinTheGame()) {
-                    consoleOutputHandler.printGameWinningComment();
+                    outputHandler.showGameWinningComment();
                     break;
                 }
                 if (doesUserLoseTheGame()) {
-                    consoleOutputHandler.pringGameLosingComment();
+                    outputHandler.showGameLosingComment();
                     break;
                 }
 
                 String cellInput = getCellInputFromUser();
                 String userActionInput = getUserActionInputFromUser();
                 actOnCell(cellInput, userActionInput);
-            } catch (GameException e) { //우리가 의도적으로 던진 예외
-                consoleOutputHandler.printExceptionMessage(e);
-            } catch (Exception e) { // 예상하지 못한 예외 처리
-                consoleOutputHandler.printSimpleMessage("프로그램에 문제가 생겼습니다.");
+            } catch (GameException e) {
+                outputHandler.showExceptionMessage(e);
+            } catch (Exception e) {
+                outputHandler.showSimpleMessage("프로그램에 문제가 생겼습니다.");
             }
         }
     }
+
     private void actOnCell(String cellInput, String userActionInput) {
         int selectedColIndex = boardIndexConverter.getSelectedColIndex(cellInput, gameBoard.getColSize());
         int selectedRowIndex = boardIndexConverter.getSelectedRowIndex(cellInput, gameBoard.getRowSize());
 
         if (doesUserChooseToPlantFlag(userActionInput)) {
-            gameBoard.flag(selectedRowIndex, selectedColIndex);// 깃발이 꽂힌 상태
+            gameBoard.flag(selectedRowIndex, selectedColIndex);
             checkIfGameIsOver();
             return;
         }
@@ -71,7 +76,7 @@ public class Minesweeper implements GameInitializable, GameRunnable {
             checkIfGameIsOver();
             return;
         }
-        throw new GameException("잘못된 번호를 선택하셨습니다."); // 의도한 예외
+        throw new GameException("잘못된 번호를 선택하셨습니다.");
     }
 
     private void changeGameStatusToLose() {
@@ -87,13 +92,13 @@ public class Minesweeper implements GameInitializable, GameRunnable {
     }
 
     private String getUserActionInputFromUser() {
-        consoleOutputHandler.printCommentForUserAction();
-        return consoleInputHandler.getUserInput();
+        outputHandler.showCommentForUserAction();
+        return inputHandler.getUserInput();
     }
 
     private String getCellInputFromUser() {
-        consoleOutputHandler.printCommentForSelectingCell();
-        return consoleInputHandler.getUserInput();
+        outputHandler.showCommentForSelectingCell();
+        return inputHandler.getUserInput();
     }
 
     private boolean doesUserLoseTheGame() {
